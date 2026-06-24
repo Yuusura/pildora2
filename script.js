8c8b442733abbc6f4902251e37633c9d85a71bc9
Yuusura <git@stackblitz.com>
2026-06-24 16:41:52 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//Declara dos variables numéricas y realiza operaciones aritméticas con ellas (suma, resta, multiplicación, división, módulo) y muestra los resultados en la consola.
+let numero1 = 8;
+const numero2 = 3;
+
+console.log('suma', numero1 + numero2);
+console.log('resta', numero1 - numero2);
+console.log('multiplicación', numero1 * numero2);
+console.log('división', numero1 / numero2);
+console.log('resto', numero1 % numero2);
+//Realiza operaciones de comparación entre las variables y muestra los resultados en la consola.
+let texto1 = '8';
+console.log('igual en contenido', numero1 == texto1);
+console.log('igual en contenido y tipo', numero1 === texto1);
+console.log('mayor que', numero1 > numero2);
+console.log('menor que', numero1 < numero2);
+console.log('mayor o igul que', numero1 >= numero2);
+console.log('menor o igual que', numero1 <= numero2);
+console.log('negación contenido', numero1 != numero2);
+console.log('negación contenido y tipo', numero1 !== numero2);
+
+let numero3;
+console.log(numero3);
+let numero4 = null;
+console.log(numero4);
+
+//Utiliza operadores lógicos para combinar condiciones y muestra los resultados en la consola.
